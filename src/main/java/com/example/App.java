032c7fa1bce29c1417fb71fa76ea32b618a90217
lewiswon <lewiswon@sina.com>
2023-03-26 10:58:38 +0800

    init

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import com.example.controller.HelloController;
+import io.bootique.BaseModule;
+import io.bootique.Bootique;
+import io.bootique.di.Binder;
+import io.bootique.jersey.JerseyModule;
+import io.bootique.jersey.jackson.JerseyJacksonModule;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends BaseModule
+{
+    public static void main( String[] args )
+    {
+        Bootique
+                .app(args)
+                .autoLoadModules()
+                .module(App.class)
+                .exec()
+                .exit();
+    }
+
+    @Override
+    public void configure(Binder binder) {
+        JerseyModule.extend(binder).addResource(HelloController.class);
+        JerseyJacksonModule.extend(binder).skipNullProperties();
+    }
+}
